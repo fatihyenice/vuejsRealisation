d2a6d656b5b43c1de62d29e6b77d509cc3c114c6
Fatih Yenice <fatih.ydev@gmail.com>
2025-06-13 09:26:57 +0200

    Création des composants Header et LinkHeader.vue

---
+++ src/components/LinkHeader.vue
@@ -0,0 +1,11 @@
+<template>
+    <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">{{ link }}</a>
+    </li> 
+</template>
+
+<script setup>
+defineProps({
+    link: String
+})
+</script>
