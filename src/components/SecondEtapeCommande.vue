da7588e8b758eef9eff1bc4dc9c24f8ebe5ee918
Fatih Yenice <fatih.ydev@gmail.com>
2025-07-03 16:36:12 +0200

    étape de livraison grosse avancé

--- src/components/SecondEtapeCommande.vue
+++ src/components/SecondEtapeCommande.vue
@@ -1,3 +1,38 @@
 <template>
-    <h2>Livraison</h2>
+    <div class="container-livraison">
+        <h2>Livraison</h2>
+
+        <div class="big-container-livraison">
+            <div class="left-livraison">
+                <form method="POST">
+                    <Livraison nomLivreur="Chronopost" @choixlivraison="ajoutPrixLivraison" :imageurl="chronopost"
+                        :prix="4.9" />
+                    <Livraison nomLivreur="DPD" @choixlivraison="ajoutPrixLivraison" :imageurl="dpd" :prix="3.9" />
+                    <Livraison nomLivreur="48H" @choixlivraison="ajoutPrixLivraison" :imageurl="quarantehuit"
+                        :prix="7.9" />
+                </form>
+            </div>
+            <div class="right-livraison">
+                TOTAL: {{ prixFinal }}€
+            </div>
+        </div>
+    </div>
 </template>
+
+<script setup>
+import { panierStore } from '@/stores/panier';
+import chronopost from '@/assets/chronopost-mini.png';
+import dpd from '@/assets/dpd.png';
+import { computed, ref } from 'vue';
+import { watch } from 'vue';
+import quarantehuit from '@/assets/48.png';
+import Livraison from './livraisons/livraison.vue';
+
+const panier = panierStore();
+
+const prixFinal = ref(panier.totalPrix);
+
+const ajoutPrixLivraison = (prix) => {
+    prixFinal.value = `${panier.totalPrix + prix.prix} commande: ${panier.totalPrix}`
+}
+</script>
---
+++ src/components/livraisons/livraison.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <input type="radio" :value="nomLivreur" :id="nomLivreur" v-model="choix" @change="envoyer" name="livraison">
+        <label :for="nomLivreur"><img :src="imageurl" alt="logo de chronopost" width="40">
+            {{ nomLivreur }}
+            {{ prix }}€</label>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+    nomLivreur: String,
+    imageurl: String,
+    prix: Number,
+})
+
+const choix = ref('')
+
+const emits = defineEmits("choixlivraison");
+
+const envoyer = () => {
+    emits('choixlivraison', { nomLivreur: choix.value, prix: props.prix })
+}
+</script>
